959e451ac363887c03870c499ac233751fd1e157
= <=>
2024-05-02 01:05:45 +0600

    Installed necessary libraries and add authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import Logo from '@/components/Logo'
+import React, { ReactNode } from 'react'
+
+function layout({children}: {children: ReactNode}) {
+  return (
+    <div className='relative h-screen w-full flex flex-col items-center justify-center'>
+      <Logo/>
+      <div className="mt-12">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default layout
